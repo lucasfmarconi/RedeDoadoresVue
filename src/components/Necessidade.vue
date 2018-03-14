c5505e08422b440b3e686ce696acab028884828a
Lucas F Marconi Ferreira <lucas.ferreira@vetta.com.br>
2018-03-14 15:29:59 -0300

    Changing aplication layout

---
+++ src/components/Necessidade.vue
@@ -0,0 +1,14 @@
+<template>
+  <section id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+          <form>
+            <input type="radio" name="gender" value="male" checked> Male<br>
+            <input type="radio" name="gender" value="female"> Female<br>
+          </form>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
